89e76acc4e3736b5444d02459dcea30140f03589
ymym <631119469@qq.com>
2015-09-16 23:47:09 +0800

    wechat actionbar

--- app/src/main/java/org/pleaseyouya/androidui/MainActivity.java
+++ app/src/main/java/org/pleaseyouya/androidui/MainActivity.java
@@ -1,62 +1,174 @@
 package org.pleaseyouya.androidui;
 
-import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.support.v7.app.ActionBarActivity;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.ViewConfiguration;
 import android.view.Window;
-import android.widget.RelativeLayout;
 
-import org.pleaseyouya.androidui.view.RoundCorner;
+import org.pleaseyouya.androidui.fragment.ChatFragment;
+import org.pleaseyouya.androidui.fragment.ContactsFragment;
+import org.pleaseyouya.androidui.fragment.FoundFragment;
+import org.pleaseyouya.androidui.view.PagerSlidingTabStrip;
 
 
-public class MainActivity extends Activity {
+/**
+ * 高仿微信的主界面
+ *
+ * http://blog.csdn.net/guolin_blog/article/details/26365683
+ *
+ * @author guolin
+ */
+public class MainActivity extends FragmentActivity {
+
+    /**
+     * 聊天界面的Fragment
+     */
+    private ChatFragment chatFragment;
+
+    /**
+     * 发现界面的Fragment
+     */
+    private FoundFragment foundFragment;
+
+    /**
+     * 通讯录界面的Fragment
+     */
+    private ContactsFragment contactsFragment;
+
+    /**
+     * PagerSlidingTabStrip的实例
+     */
+    private PagerSlidingTabStrip tabs;
+
+    /**
+     * 获取当前屏幕的密度
+     */
+    private DisplayMetrics dm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
-
-        RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.main);
-
-        // 从一个drawable获取bitmap
-        // 首先将drawable强制转换为BitmapDrawable对象
-        // 然后从BitmapDrawable获取bitmap
-        Drawable drawable = getResources().getDrawable(R.mipmap.background);
-        BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
-        Bitmap bitmap = bitmapDrawable.getBitmap();
-
-        // 通过一个Bitmap对象创建BitmapDrawable
-        BitmapDrawable newBitmapDrawable = new BitmapDrawable(RoundCorner.toRoundCorner(bitmap,
-                160));
-        relativeLayout.setBackgroundDrawable(newBitmapDrawable);
+        setContentView(R.layout.wechat_main);
+        setOverflowShowingAlways();
+        dm = getResources().getDisplayMetrics();
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+        tabs.setViewPager(pager);
+        setTabsValue();
     }
 
+    /**
+     * 对PagerSlidingTabStrip的各项属性进行赋值。
+     */
+    private void setTabsValue() {
+        // 设置Tab是自动填充满屏幕的
+        tabs.setShouldExpand(true);
+        // 设置Tab的分割线是透明的
+        tabs.setDividerColor(Color.TRANSPARENT);
+        // 设置Tab底部线的高度
+        tabs.setUnderlineHeight((int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, 1, dm));
+        // 设置Tab Indicator的高度
+        tabs.setIndicatorHeight((int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, 4, dm));
+        // 设置Tab标题文字的大小
+        tabs.setTextSize((int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_SP, 16, dm));
+        // 设置Tab Indicator的颜色
+        tabs.setIndicatorColor(Color.parseColor("#45c01a"));
+        // 设置选中Tab文字的颜色 (这是我自定义的一个方法)
+        tabs.setSelectedTextColor(Color.parseColor("#45c01a"));
+        // 取消点击Tab时的背景色
+        tabs.setTabBackground(0);
+    }
+
+    public class MyPagerAdapter extends FragmentPagerAdapter {
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        private final String[] titles = { "聊天", "发现", "通讯录" };
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+
+        @Override
+        public int getCount() {
+            return titles.length;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    if (chatFragment == null) {
+                        chatFragment = new ChatFragment();
+                    }
+                    return chatFragment;
+                case 1:
+                    if (foundFragment == null) {
+                        foundFragment = new FoundFragment();
+                    }
+                    return foundFragment;
+                case 2:
+                    if (contactsFragment == null) {
+                        contactsFragment = new ContactsFragment();
+                    }
+                    return contactsFragment;
+                default:
+                    return null;
+            }
+        }
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public boolean onMenuOpened(int featureId, Menu menu) {
+        if (featureId == Window.FEATURE_ACTION_BAR && menu != null) {
+            if (menu.getClass().getSimpleName().equals("MenuBuilder")) {
+                try {
+                    Method m = menu.getClass().getDeclaredMethod(
+                            "setOptionalIconsVisible", Boolean.TYPE);
+                    m.setAccessible(true);
+                    m.invoke(menu, true);
+                } catch (Exception e) {
+                }
+            }
         }
-
-        return super.onOptionsItemSelected(item);
+        return super.onMenuOpened(featureId, menu);
     }
+
+    private void setOverflowShowingAlways() {
+        try {
+            ViewConfiguration config = ViewConfiguration.get(this);
+            Field menuKeyField = ViewConfiguration.class
+                    .getDeclaredField("sHasPermanentMenuKey");
+            menuKeyField.setAccessible(true);
+            menuKeyField.setBoolean(config, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
